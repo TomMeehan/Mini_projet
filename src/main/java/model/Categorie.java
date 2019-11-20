8285ea39c3a0eeb1f4dc448d41bad659f5f9b1a1
pedago <pedago@pcalb-mm0410.univ-jfc.fr>
2019-11-20 14:12:30 +0100

    Arborescence

---
+++ src/main/java/model/Categorie.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author pedago
+ */
+public class Categorie {
+    
+    private int code;
+    private String libelle;
+    private String description;
+
+    public Categorie(int code, String libelle, String description) {
+        this.code = code;
+        this.libelle = libelle;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+}
---
+++ src/main/java/model/Ligne.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author pedago
+ */
+public class Ligne {
+    
+    private int commande;
+    private int produit;
+    
+    private int quantite;
+
+    public Ligne(int commande, int produit, int quantite) {
+        this.commande = commande;
+        this.produit = produit;
+        this.quantite = quantite;
+    }
+
+    public int getCommande() {
+        return commande;
+    }
+
+    public void setCommande(int commande) {
+        this.commande = commande;
+    }
+
+    public int getProduit() {
+        return produit;
+    }
+
+    public void setProduit(int produit) {
+        this.produit = produit;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+    
+    
+    
+}
