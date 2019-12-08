4c73a48a39ebde82d55dfc3f6ddb7d7829913009
Tom <tom.meehan@etud.univ-jfc.fr>
2019-12-08 15:17:42 +0100

    Systeme de login fonctionnel ! Creation/destruction de session, verification que l'user est dans la BDD

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -8,7 +8,6 @@ package servlet;
 import beans.User;
 import forms.LoginForm;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
