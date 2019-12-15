b76d271c4b27ad0ff52d38301cb4d35ae5330e74
Tom <tom.meehan@etud.univ-jfc.fr>
2019-12-15 10:45:24 +0100

    add db heroku

--- src/main/java/model/DataSourceFactory.java
+++ src/main/java/model/DataSourceFactory.java
@@ -13,17 +13,40 @@ import javax.sql.DataSource;
  */
 public class DataSourceFactory {
     
-    public static DataSource getDataSource () {
-        
-        org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
-        ds.setDatabaseName("comptoirs");
-        ds.setUser("god");
-        ds.setPassword("god");
-        ds.setServerName("localhost");
-        ds.setPortNumber(1527);
-        
-        return ds;
-    }
+	public enum DriverType {
+		embedded, server
+	};
+	
+	// Choic du type de driver : embedded ou serveur
+	private static final DriverType TYPE = DriverType.embedded;
+	/**
+	 * Renvoie la source de données (server ou embbeded)
+	 * @return  la source de données
+	 */
+	public static DataSource getDataSource() {
+            DataSource result;
+
+            switch (TYPE) {
+                    case server: // Derby mode serveur, doit être démarré indépendamment
+                            org.apache.derby.jdbc.ClientDataSource ds = new org.apache.derby.jdbc.ClientDataSource();
+                            ds.setDatabaseName("comptoirs");
+                            ds.setUser("god");
+                            ds.setPassword("god");
+                            // The host on which Network Server is running
+                            ds.setServerName("localhost");
+                            // port on which Network Server is listening
+                            ds.setPortNumber(1527);
+                            result = ds;
+                            break;
+                    default: // Derby mode embedded, démarré automatiquement avec l'application
+                            org.apache.derby.jdbc.EmbeddedDataSource es = new org.apache.derby.jdbc.EmbeddedDataSource();
+                            es.setCreateDatabase("create");
+                            es.setDatabaseName("embedded_sample");
+                            result = es;
+            }
+
+            return result;
+	}
 
 
 }
